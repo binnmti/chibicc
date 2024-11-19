7557e83b39eb4b01d000faf4e72308b3c10019f3
binnmti <binnmtiisprogrammer@gmail.com>
2024-11-19 11:18:06 +0900

    単項プラスと単項マイナス

--- 9cc.c
+++ 9cc.c
@@ -8,6 +8,7 @@
 //
 // Tokenizer
 //
+
 typedef enum {
   TK_RESERVED, // Keywords or punctuators
   TK_NUM,      // Integer literals
@@ -126,6 +127,7 @@ Token *tokenize() {
 //
 // Parser
 //
+
 typedef enum {
   ND_ADD, // +
   ND_SUB, // -
@@ -133,6 +135,7 @@ typedef enum {
   ND_DIV, // /
   ND_NUM, // Integer
 } NodeKind;
+
 // AST node type
 typedef struct Node Node;
 struct Node {
@@ -141,28 +144,35 @@ struct Node {
   Node *rhs;     // Right-hand side
   int val;       // Used if kind == ND_NUM
 };
+
 Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
   return node;
 }
+
 Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = new_node(kind);
   node->lhs = lhs;
   node->rhs = rhs;
   return node;
 }
+
 Node *new_num(int val) {
   Node *node = new_node(ND_NUM);
   node->val = val;
   return node;
 }
+
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
+
 // expr = mul ("+" mul | "-" mul)*
 Node *expr() {
   Node *node = mul();
+
   for (;;) {
     if (consume('+'))
       node = new_binary(ND_ADD, node, mul());
@@ -172,17 +182,29 @@ Node *expr() {
       return node;
   }
 }
-// mul = primary ("*" primary | "/" primary)*
+
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
+
   for (;;) {
     if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
+      node = new_binary(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
+      node = new_binary(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+// unary = ("+" | "-")? unary
+//       | primary
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_binary(ND_SUB, new_num(0), unary());
+  return primary();
 }
 // primary = "(" expr ")" | num
 Node *primary() {
@@ -191,20 +213,26 @@ Node *primary() {
     expect(')');
     return node;
   }
+
   return new_num(expect_number());
 }
+
 //
 // Code generator
 //
+
 void gen(Node *node) {
   if (node->kind == ND_NUM) {
     printf("  push %d\n", node->val);
     return;
   }
+
   gen(node->lhs);
   gen(node->rhs);
+
   printf("  pop rdi\n");
   printf("  pop rax\n");
+
   switch (node->kind) {
   case ND_ADD:
     printf("  add rax, rdi\n");
@@ -220,8 +248,10 @@ void gen(Node *node) {
     printf("  idiv rdi\n");
     break;
   }
+
   printf("  push rax\n");
 }
+
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
